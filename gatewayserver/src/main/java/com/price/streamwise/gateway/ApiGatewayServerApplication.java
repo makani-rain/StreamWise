fcb604b51950a42171de8272e4039bc326aff992
Rain <makani.rain@gmail.com>
2025-11-04 15:25:30 -0700

    incremental changes

--- gatewayserver/src/main/java/com/price/streamwise/gateway/ApiGatewayServerApplication.java
+++ gatewayserver/src/main/java/com/price/streamwise/gateway/ApiGatewayServerApplication.java
@@ -1,4 +1,4 @@
-package com.optimagrowth.gateway;
+package com.price.streamwise.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
