0ed756ec264e64cca4678e7b7b2875a874d9f8d1
Rain <makani.rain@gmail.com>
2025-11-04 16:12:16 -0700

    Service Discovery support

--- catalog-service/src/main/java/com/price/streamwise/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/price/streamwise/catalog/CatalogServiceApplication.java
@@ -1,13 +1,39 @@
 package com.price.streamwise.catalog;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import com.price.streamwise.catalog.utils.UserContextInterceptor;
 
 @SpringBootApplication
+@RefreshScope
+@EnableFeignClients
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		RestTemplate template = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+		if (interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+
+		return template;
+	}
 }
